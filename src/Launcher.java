26ea5c45ec9290fb7af21bcd0634a433c4844c84
Lairopw <vincentvoisin28@gmail.com>
2022-09-20 15:22:03 +0200

    part one

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,13 +1,32 @@
 import java.util.Scanner;
 public class Launcher {
+    public static int fibo(int n){
+        int f0 = 0;
+        int f1 = 1;
+        int fn = -1;
+        if (n == 0){
+            fn = f0;
+        }
+        if (n == 1){
+            fn = f1;
+        }
+        for (int k = 2; k <= n; ++k){
+            fn = f1 + f0;
+            f0 = f1;
+            f1 = fn;
+        }
+        return fn;
+    }
     public static void main(String[] args) {
         System.out.println("Bienvenue");
-
         var scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!"quit".equals(input)) {
-            System.out.println("unknown command");
-            input=scanner.nextLine();
+            if("fibo".equals(input)) {
+                System.out.println("entre un nombre");
+                int n = scanner.nextInt();
+                System.out.println("fibonnaci de " + n + " est : " + fibo(n));
+            }
         }
     }
 }
--- src/fibo.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-public class fibo {
-    public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        
-    }
-}
